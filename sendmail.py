22f57db95010e7b20f13baa29e2f84d9443e811c
breno da silva <breno da silva>
2019-03-19 12:03:15 -0300

    modificado e refatorado

--- sendmail.py
+++ sendmail.py
@@ -13,6 +13,27 @@ YAHOO = 'smtp.mail.yahoo.com:587'
 BOL = 'smtps.bol.com.br:587'
 IG = 'smtp.ig.com.br:587'
 
+dicionario_servidores = {
+    'gmail.com': GMAIL,
+    
+    'yahoo.com.br': YAHOO,
+    'yahoo.com': YAHOO,
+    
+    'ig.com.br': IG,
+
+    'bol.com.br': BOL,
+    'bol.com': BOL,
+
+    'outlook.com.br': MICROSOFT,
+    'outlook.com': MICROSOFT,
+
+    'hotmail.com.br': MICROSOFT,
+    'hotmail.com': MICROSOFT,
+    
+    'live.com.br': MICROSOFT,
+    'live.com': MICROSOFT
+}
+
 if name == 'nt':
     system('color 02')
     system('exit')
@@ -49,41 +70,23 @@ def inserir_dados():
     else:
         servidor = seu_email[arroba_index + 1:]
 
-    if servidor == 'hotmail.com' or servidor == 'hotmail.com.br' or servidor == 'live.com.br' or \
-            servidor == 'live.com' or servidor == 'outlook.com' or servidor == 'outlook.com.br':
+    if servidor in dicionario_servidores:
+        SERVIDOR = dicionario_servidores[servidor]
+
         try:
-            login_em_servidor(MICROSOFT)
+            if SERVIDOR == GMAIL:
+                try:
+                    login_em_servidor(GMAIL)
+                except:
+                    print "Erro!\nPrimeiro permita que o login em aplicativos menos seguros aqui: goo.gl/IRDi9y"
+                    inserir_dados()
+            else:
+                login_em_servidor(SERVIDOR)
+
         except s.SMTPAuthenticationError:
             print "E-mail ou senha errados..."
             inserir_dados()
-    elif servidor == 'gmail.com':
-        try:
-            try:
-                login_em_servidor(GMAIL)
-            except:
-                print "Erro!\nPrimeiro permita que o login em aplicativos menos seguros aqui: goo.gl/IRDi9y"
-                inserir_dados()
-        except s.SMTPAuthenticationError:
-            print "E-mail ou senha errados..."
-            inserir_dados()
-    elif servidor == 'yahoo.com' or servidor == 'yahoo.com.br':
-        try:
-            login_em_servidor(YAHOO)
-        except s.SMTPAuthenticationError:
-            print "E-mail ou senha errados..."
-            inserir_dados()
-    elif servidor == 'bol.com.br' or servidor == 'bol.com':
-        try:
-            login_em_servidor(BOL)
-        except s.SMTPAuthenticationError:
-            print "E-mail ou senha errados..."
-            inserir_dados()
-    elif servidor == 'ig.com.br':
-        try:
-            login_em_servidor(IG)
-        except s.SMTPAuthenticationError:
-            print "E-mail ou senha errados..."
-            inserir_dados()
+
     else:
         system('cls')
         print 'Ainda não suportamos esse servidor de e-mail, tente com outro.'
@@ -128,6 +131,7 @@ try:
         sleep(180)
 except KeyboardInterrupt:
     print("Muito obrigado por usar o FenixFlooder v1.0")
-    input('')
-
-obj.quit()
+    try:
+        input('')
+    except:
+        obj.quit()
